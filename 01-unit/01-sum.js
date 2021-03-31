1b5ee296baebffbc9d24daeae4acb77f2795dd18
julie ugolotti <julie.ugolotti@gmail.com>
2021-03-31 16:02:06 -0400

    Exercise part 1

---
+++ 01-unit/01-sum.js
@@ -0,0 +1,48 @@
+// ==================================================
+// DEFINE FUNCTION(S)
+// ==================================================
+function sum(num1, num2) {
+return num1 + num2;
+}
+
+// ==================================================
+// DEFINE AND EXECUTE TEST CASES
+// ==================================================
+try {
+  // --------------------------------------------------
+  // Test Case 1
+  // --------------------------------------------------
+  // It should return the sum of two numbers.
+  var result = sum(1, 1);
+  if (result !== 2) throw new Error('Expected sum(1, 1) to be 2. Received: ' + result);
+
+  // --------------------------------------------------
+  // Test Case 2
+  // --------------------------------------------------
+  // It should ignore additional numbers.
+  var result = sum(1, 1, 2, 3, 4);
+  if (result !== 2) throw new Error('Expected sum(1, 1, 2, 3, 4) to be 2. Received: ' + result);
+
+  // --------------------------------------------------
+  // Test Case 3
+  // --------------------------------------------------
+  // When invoked with only 1 number, it should return that number.
+  var result = sum(1);
+  if (result !== 1) throw new Error('Expected sum(1) to be 1. Received: ' + result);
+  
+  // --------------------------------------------------
+  // Test Case 4
+  // --------------------------------------------------
+  // When invoked with no numbers, it should return 0.
+  var result = sum();
+  if (result !== 0) throw new Error('Expected sum() to be 0. Received: ' + result);
+
+  console.log('All tests passed successfully.');
+
+// ==================================================
+// PRINT ERRORS
+// ==================================================
+} catch (e) {
+  console.warn('Whoops, the following test did not pass:');
+  console.error(e.message);
+}
---
+++ 01-unit/03-product.js
@@ -0,0 +1,55 @@
+// ==================================================
+// DEFINE FUNCTION(S)
+// ==================================================
+function product(num1, num2) {
+return num1 * num2;
+}
+
+// ==================================================
+// DEFINE AND EXECUTE TEST CASES
+// ==================================================
+try {
+  // --------------------------------------------------
+  // Test Case 1
+  // --------------------------------------------------
+  // It should return the product of two numbers.
+  var result = product(2, 2);
+  if (result !== 4) throw new Error('Expected product(2, 2) to be 4. Received: ' + result);
+
+  // --------------------------------------------------
+  // Test Case 2
+  // --------------------------------------------------
+  // It should return 0 when either of the numbers are 0.
+  var result = product(0, 2);
+  if (result !== 0) throw new Error('Expected product(0, 2) to be 0. Received: ' + result);
+
+  // --------------------------------------------------
+  // Test Case 3
+  // --------------------------------------------------
+  // It should ignore additional numbers.
+  var result = product(2, 2, 3, 4);
+  if (result !== 4) throw new Error('Expected product(2, 2, 3, 4) to be 4. Received: ' + result);
+
+  // --------------------------------------------------
+  // Test Case 4
+  // --------------------------------------------------
+  // When invoked with only 1 number, it should return that number.
+  var result = product(2);
+  if (result !== 2) throw new Error('Expected product(2) to be 2. Received: ' + result);
+
+  // --------------------------------------------------
+  // Test Case 5
+  // --------------------------------------------------
+  // When invoked with no numbers, it should return 0.
+  var result = product();
+  if (result !== 0) throw new Error('Expected product() to be 0. Received: ' + result);
+
+  console.log('All tests passed successfully.');
+
+// ==================================================
+// PRINT ERRORS
+// ==================================================
+} catch (e) {
+  console.warn('Whoops, the following test did not pass:');
+  console.error(e.message);
+}
